e1f5937f118a50aeb56133c8f8640d29672cd58e
DavideBasile1810355 <60829979+dave0909@users.noreply.github.com>
2021-11-14 16:38:06 +0100

    Revert "Update FIX hw1"
    This reverts commit 53cad45cf3d79d973707ae3939c98982f868f9c7.

--- DroNETworkSimulator-hmw1/src/simulation/simulator.py
+++ DroNETworkSimulator-hmw1/src/simulation/simulator.py
@@ -226,13 +226,13 @@ class Simulator:
 
             if cur_step % 10000 == 0:
                 end = time.time()
-                print("step: " + str(cur_step), time.strftime("%H:%M:%S", time.gmtime(end - self.start)))
+                print("step: " + str(cur_step), time.strftime("%H:%M:%S", time.gmtime(end - self.start)),"-----------------------------------------------------------------------------------")
+                self.start = time.time()
                 try:
                     self.print_metrics()
                 except:
-                    print("except")
-                self.start = time.time()
-
+                    print(None)
+                    
             if self.show_plot or config.SAVE_PLOT:
                 self.__plot(cur_step)
 
